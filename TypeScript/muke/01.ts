44aef6b27d4d5cb3ef101e21c81c50f027ad47ab
Daniel Qinqing Zhou <daniel_qinqing_zhou@apple.com>
2023-03-14 15:09:00 +0800

    add TypeScript file

---
+++ TypeScript/muke/01.ts
@@ -0,0 +1,5 @@
+// TypeScript 2012 年由 微软正式发布，包括 Angular、React、Vue 等广泛使用的前端框架，都在使用 TypeScript 进行开发，
+// TypeScript 是 javascript 的超集，这意味着它是建立在 JavaScript 语法上的一门语言。
+// TypeScript 把其它语言上的一些精妙的语法，带入到了 JavaScript 之中，从而把 js 带到了一个新的高度。
+// 你可以在 ts 里面，使用各种的， js 之外的扩展语法，同时借助 ts 对面向对象和静态类型的良好支持，你可以编写出更健壮，和更可维护的大型项目。
+// 
